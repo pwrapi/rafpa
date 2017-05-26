034d139e4b8e60d4bdc72792476f66d28e3b2258
Vinanti Phadke <vinanti.phadke@hpe.com>
2017-05-27 04:39:07 +0530

    script change

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -3,4 +3,4 @@ SCRIPT_PATH=$(dirname $0)
 ROOT=$SCRIPT_PATH/..
 export REDFISH_AGENT_ROOT=$ROOT
 export PYTHONPATH=${ROOT}/lib/python
-python ${ROOT}/lib/python/agent.py
+python ${ROOT}/lib/python/proxy.py
