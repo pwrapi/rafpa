51714ebd8652d06f1a03fe2625e964074663b7bc
root <prasanth kurian>
2017-08-11 14:41:33 +0000

    adding copyright 1

--- lib/python/Util.py
+++ lib/python/Util.py
@@ -1,8 +1,8 @@
 # © Copyright 2017 Hewlett Packard Enterprise Development LP
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-# 1. Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,this list
+# of conditions and the following disclaimer.
 # 2. Redistributions in binary form must reproduce the above copyright notice, this
 # list of conditions and the following disclaimer in the documentation and/or other
 # materials provided with the distribution.
