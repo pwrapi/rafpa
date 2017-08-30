0ecb5c861ff072a875b6e338dd26c3e2c81e8879
Vinanti Phadke <vinanti.phadke@hpe.com>
2017-08-30 08:48:57 +0530

    Final Changes to be Pushed to Repo

--- bin/proxy.sh
+++ bin/proxy.sh
@@ -24,5 +24,4 @@ SCRIPT_PATH=$(dirname $0)
 ROOT=$SCRIPT_PATH/..
 #export REDFISH_AGENT_ROOT=$ROOT
 export PYTHONPATH=${ROOT}/lib/python
-#python ${ROOT}/lib/python/proxy.py
 python ${ROOT}/lib/python/myDaemon.py ${ROOT} $*
