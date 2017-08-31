46d442f2d09b3b8796d3d2a8eb8df58a4d174b9b
Vinanti Phadke <vinanti.phadke@hpe.com>
2017-09-01 04:42:32 +0530

    Code sanitaisation of Install script

--- install.sh
+++ install.sh
@@ -7,9 +7,20 @@ if [[ $COUNT -lt 1 ]]
 then
    echo "Not enough arguments Please specify directory for PowerAPI Path if already installed"
    exit
-fi 
+fi
+
+#checking if PowerAPI is existing on your system
+echo "Checking if PowerAPI Present on your system"
+
+if [ -d "$PowerDIR" ];then
+   echo "PowerAPI Installed on the System. - Yes"
+else
+   echo "Incorrect PowerAPI Directory Specified. Please Specify the correct Directory"
+   exit
+fi
+
 #checking OS Version
-echo "checking OS Version"
+echo "Checking OS Version"
 OSV=$(lsb_release -si)
 echo $OSV
 UbOS='Ubuntu'
@@ -23,78 +34,78 @@ else
    exit
 fi
 #checking for git 
-echo "checking git"
+echo "Checking git"
 if hash git 2>/dev/null; then
-   echo "Git already installed"
+   echo "Git already installed. - Yes"
 else
    echo "Git not installed hence installing"
    $command install git
 fi
 
 #checking for pip
-echo "checking for pip"
+echo "Checking for pip"
 
 if hash pip 2>/dev/null; then
-   echo "pip already installed"
+   echo "pip already installed. - Yes"
 else
    echo "pip not installed hence installing"
    $command install python-pip
 fi
 
 #checking for python and installed versions
-echo "checking for python and installed versions"
+echo "Checking for python and installed versions"
 V=$(pip show Python | grep Version | cut -c10-|cut -d"."  -f1,2)
 #V=$(pip show Python | grep Version | cut -c10-)
-echo $V
+#echo $V
 
 if [[ -z "$V" ]]; then
    echo "Python not installed hence exiting"
    exit
 fi
 q=$(echo $V | awk '{ print ($1 >= 2.7 && $1 < 3 ) ? "true" : "false" }')
-echo $q
+#echo $q
 
 if [ "$q" == "true" ]; then
-   echo "Correct Version installed"
+   echo "Correct Version installed - Yes"
 else
    echo "Incorrect Version of Python Installed hence exiting Please Install Version Greater than 2.7 and Less than 3.0"
    exit
 fi
 
 #checking for sushy
-echo "checking for sushy"
+echo "Checking for sushy"
 A=$(pip show sushy)
-echo $A	
+#echo $A	
 if [[ -z "$A" ]]; then
    echo "Sushy not installed. Installing sushy"
    pip install sushy
 #exit
 else
-   echo "Sushy already installed"
+   echo "Sushy already installed - Yes"
 fi
  
 #checking for yaml
-echo "checking for yaml"
+echo "Checking for yaml"
 A=$(pip show pyyaml)
-echo $A	
+#echo $A	
 if [[ -z "$A" ]]; then
    echo "Yaml not installed. Installing Yaml"
    pip install pyyaml
 #exit
 else
-   echo "Yaml already installed"
+   echo "Yaml already installed - Yes"
 fi
 
 #checking for python sdk   
-echo "checking for python sdk"
+echo "Checking for python sdk"
 A=$(pip show python-ilorest-library)
-echo $A	
+#echo $A	
 if [[ -z "$A" ]]; then
    echo "Python sdk not installed. Please install Python SDK for In-Band-Communication"
    echo "After installation please place the device .so file in your working directory"
    echo "You can download the so file from the location specfied in the README "
 else
-   echo "Correct Version of Python sdk installed"
+   echo "Correct Version of Python sdk installed - Yes"
 fi
 
 echo "Installation Completed!!!"
